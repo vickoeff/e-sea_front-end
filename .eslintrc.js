5cb180d5aed736f63bdec54ed4ac21f49ae0c703
vickoeff <vic.eff3@gmail.com>
2023-10-25 18:32:59 +0700

    update setup env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
-    extends: [
-      // add more generic rulesets here, such as:
-      // 'eslint:recommended',
-      // 'plugin:vue/vue2-recommended',
-      'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
-    ],
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
-  }
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    // 'plugin:vue/vue2-recommended',
+    "plugin:vue/recommended", // Use this if you are using Vue.js 2.x.
+  ],
+  plugins: ["prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    "vue/html-self-closing": 0,
+  },
+};
--- src/views/commons/NavBar.vue
+++ src/views/commons/NavBar.vue
@@ -1,134 +1,301 @@
 <template>
   <header class="header">
-    <nav class="navbar navbar-expand-lg fixed-top" v-bind:class="{
-      affix: hasAffix,
-      'custom-nav': coloredLogo,
-      'bg-transparent': !coloredLogo,
-    }">
+    <nav
+      class="navbar navbar-expand-lg fixed-top"
+      :class="{
+        affix: hasAffix,
+        'custom-nav': coloredLogo,
+        'bg-transparent': !coloredLogo
+      }"
+    >
       <div class="container">
-        <a class="navbar-brand" href="/">
-          <img :src="coloredLogo
-            ? '../../assets/img/logo-color.png'
-            : '../../assets/img/logo-white.png'
-            " alt="logo" class="img-fluid" />
+        <a
+          class="navbar-brand"
+          href="/"
+        >
+          <img
+            src="../../assets/img/logo.png"
+            alt="logo"
+            class="img-fluid"
+          />
         </a>
-        <button class="navbar-toggler" type="button" @click="mobileNavClicked" v-bind:class="{ collapsed: collapsed }">
-          <span class="ti-menu"></span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          :class="{ collapsed: collapsed }"
+          @click="mobileNavClicked"
+        >
+          <span class="ti-menu" />
         </button>
-        <div class="collapse navbar-collapse h-auto" v-bind:class="{ show: !collapsed }" id="navbarSupportedContent">
+        <div
+          id="navbarSupportedContent"
+          class="collapse navbar-collapse h-auto"
+          :class="{ show: !collapsed }"
+        >
           <ul class="navbar-nav ml-auto menu">
             <li>
               <router-link to="/">Home</router-link>
             </li>
             <li>
-              <router-link to="#about" v-scroll-to="'#about'">About</router-link>
+              <router-link
+                v-scroll-to="'#about'"
+                to="#about"
+              >
+                About
+              </router-link>
             </li>
             <li>
-              <router-link to="#features" v-scroll-to="'#features'">Features</router-link>
+              <router-link
+                v-scroll-to="'#features'"
+                to="#features"
+              >
+                Features
+              </router-link>
             </li>
             <li>
-              <a href="#" class="dropdown-toggle">Pages</a>
+              <a
+                href="#"
+                class="dropdown-toggle"
+              >
+                Pages
+              </a>
               <ul class="sub-menu">
                 <li>
-                  <a href="#" class="dropdown-toggle-inner">Login & Sign Up</a>
+                  <a
+                    href="#"
+                    class="dropdown-toggle-inner"
+                  >
+                    Login & Sign Up
+                  </a>
                   <ul class="sub-menu">
                     <li>
-                      <router-link to="login" v-scroll-to="'#login'">Login Page</router-link>
+                      <router-link
+                        v-scroll-to="'#login'"
+                        to="login"
+                      >
+                        Login Page
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="sign-up" v-scroll-to="'#sign-up'">Signup Page</router-link>
+                      <router-link
+                        v-scroll-to="'#sign-up'"
+                        to="sign-up"
+                      >
+                        Signup Page
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="reset" v-scroll-to="'#reset'">Reset Password</router-link>
+                      <router-link
+                        v-scroll-to="'#reset'"
+                        to="reset"
+                      >
+                        Reset Password
+                      </router-link>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <a href="#" class="dropdown-toggle-inner">Utilities</a>
+                  <a
+                    href="#"
+                    class="dropdown-toggle-inner"
+                  >
+                    Utilities
+                  </a>
                   <ul class="sub-menu">
                     <li>
-                      <router-link to="faq" v-scroll-to="'#faq'">FAQ Page</router-link>
+                      <router-link
+                        v-scroll-to="'#faq'"
+                        to="faq"
+                      >
+                        FAQ Page
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="404" v-scroll-to="'#404'">404 Page</router-link>
+                      <router-link
+                        v-scroll-to="'#404'"
+                        to="404"
+                      >
+                        404 Page
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="coming-soon" v-scroll-to="'#coming-soon'">Coming Soon</router-link>
+                      <router-link
+                        v-scroll-to="'#coming-soon'"
+                        to="coming-soon"
+                      >
+                        Coming Soon
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="thank-you" v-scroll-to="'#thank-you'">Thank You</router-link>
+                      <router-link
+                        v-scroll-to="'#thank-you'"
+                        to="thank-you"
+                      >
+                        Thank You
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="download" v-scroll-to="'#download'">Download Page
-                        <span class="badge accent-bg text-white">New</span></router-link>
+                      <router-link
+                        v-scroll-to="'#download'"
+                        to="download"
+                      >
+                        Download Page
+                        <span class="badge accent-bg text-white">New</span>
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="review" v-scroll-to="'#review'">Review Page
-                        <span class="badge accent-bg text-white">New</span></router-link>
+                      <router-link
+                        v-scroll-to="'#review'"
+                        to="review"
+                      >
+                        Review Page
+                        <span class="badge accent-bg text-white">New</span>
+                      </router-link>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <a href="#" class="dropdown-toggle-inner">Team</a>
+                  <a
+                    href="#"
+                    class="dropdown-toggle-inner"
+                  >
+                    Team
+                  </a>
                   <ul class="sub-menu">
                     <li>
-                      <router-link to="team" v-scroll-to="'#team'">Our Team Members</router-link>
+                      <router-link
+                        v-scroll-to="'#team'"
+                        to="team"
+                      >
+                        Our Team Members
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="profile" v-scroll-to="'#profile'">Team Member Profile</router-link>
+                      <router-link
+                        v-scroll-to="'#profile'"
+                        to="profile"
+                      >
+                        Team Member Profile
+                      </router-link>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <a href="#" class="dropdown-toggle-inner">Our Blog</a>
+                  <a
+                    href="#"
+                    class="dropdown-toggle-inner"
+                  >
+                    Our Blog
+                  </a>
                   <ul class="sub-menu">
                     <li>
-                      <router-link to="blog-grid" v-scroll-to="'#blog-grid'">Blog Grid</router-link>
+                      <router-link
+                        v-scroll-to="'#blog-grid'"
+                        to="blog-grid"
+                      >
+                        Blog Grid
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="blog-large" v-scroll-to="'#blog-large'">Blog No Sidebar</router-link>
+                      <router-link
+                        v-scroll-to="'#blog-large'"
+                        to="blog-large"
+                      >
+                        Blog No Sidebar
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="blog-left-sidebar" v-scroll-to="'#blog-left-sidebar'">Blog Left
-                        Sidebar</router-link>
+                      <router-link
+                        v-scroll-to="'#blog-left-sidebar'"
+                        to="blog-left-sidebar"
+                      >
+                        Blog Left Sidebar
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="blog-right-sidebar" v-scroll-to="'#blog-right-sidebar'">Blog Right
-                        Sidebar</router-link>
+                      <router-link
+                        v-scroll-to="'#blog-right-sidebar'"
+                        to="blog-right-sidebar"
+                      >
+                        Blog Right Sidebar
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="blog-details-left-sidebar" v-scroll-to="'#blog-details-left-sidebar'">Blog Detail
-                        Left Sidebar</router-link>
+                      <router-link
+                        v-scroll-to="'#blog-details-left-sidebar'"
+                        to="blog-details-left-sidebar"
+                      >
+                        Blog Detail Left Sidebar
+                      </router-link>
                     </li>
                     <li>
-                      <router-link to="blog-details-right-sidebar" v-scroll-to="'#blog-details-right-sidebar'">Blog Detail
-                        Right Sidebar</router-link>
+                      <router-link
+                        v-scroll-to="'#blog-details-right-sidebar'"
+                        to="blog-details-right-sidebar"
+                      >
+                        Blog Detail Right Sidebar
+                      </router-link>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <router-link to="about-us" v-scroll-to="'#about-us'">About Us</router-link>
+                  <router-link
+                    v-scroll-to="'#about-us'"
+                    to="about-us"
+                  >
+                    About Us
+                  </router-link>
                 </li>
                 <li>
-                  <router-link to="contact-us" v-scroll-to="'#contact-us'">Contact Us</router-link>
+                  <router-link
+                    v-scroll-to="'#contact-us'"
+                    to="contact-us"
+                  >
+                    Contact Us
+                  </router-link>
                 </li>
                 <li>
-                  <router-link to="invoice" v-scroll-to="'#invoice'">Sale Invoice
-                    <span class="badge badge-danger">New</span></router-link>
+                  <router-link
+                    v-scroll-to="'#invoice'"
+                    to="invoice"
+                  >
+                    Sale Invoice
+                    <span class="badge badge-danger">New</span>
+                  </router-link>
                 </li>
               </ul>
             </li>
             <li>
-              <router-link to="#screenshots" v-scroll-to="'#screenshots'">Screenshots</router-link>
+              <router-link
+                v-scroll-to="'#screenshots'"
+                to="#screenshots"
+              >
+                Screenshots
+              </router-link>
             </li>
             <li>
-              <router-link to="#process" v-scroll-to="'#process'">Process</router-link>
+              <router-link
+                v-scroll-to="'#process'"
+                to="#process"
+              >
+                Process
+              </router-link>
             </li>
             <li>
-              <router-link to="#pricing" v-scroll-to="'#pricing'">Pricing</router-link>
+              <router-link
+                v-scroll-to="'#pricing'"
+                to="#pricing"
+              >
+                Pricing
+              </router-link>
             </li>
             <li>
-              <router-link to="#contact" v-scroll-to="'#contact'">Contact</router-link>
+              <router-link
+                v-scroll-to="'#contact'"
+                to="#contact"
+              >
+                Contact
+              </router-link>
             </li>
           </ul>
         </div>
@@ -142,33 +309,33 @@ export default {
   props: {
     coloredLogo: {
       type: Boolean,
-      default: false,
-    },
+      default: false
+    }
   },
   data: function () {
     return {
       windowTop: 0,
-      collapsed: true,
-    };
-  },
-  mounted() {
-    window.addEventListener("scroll", this.onScroll);
-  },
-  beforeDestroy() {
-    window.removeEventListener("scroll", this.onScroll);
-  },
-  methods: {
-    onScroll: function () {
-      this.windowTop = window.top.scrollY;
-    },
-    mobileNavClicked: function () {
-      this.collapsed = !this.collapsed;
-    },
+      collapsed: true
+    }
   },
   computed: {
     hasAffix: function () {
-      return this.windowTop > 0;
-    },
+      return this.windowTop > 0
+    }
   },
-};
+  mounted() {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll: function () {
+      this.windowTop = window.top.scrollY
+    },
+    mobileNavClicked: function () {
+      this.collapsed = !this.collapsed
+    }
+  }
+}
 </script>
